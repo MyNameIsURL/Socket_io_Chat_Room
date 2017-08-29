17127aa56a680ace597ec42abc7a202f0c06daf5
Patrick Ayres <briceayres@gmail.com>
2017-08-28 22:42:17 -0500

    moved io logic inside of listen callback

--- index.js
+++ index.js
@@ -14,21 +14,20 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-
-io.on('connection', function (socket) {
-  console.log("USER CONNECTED...");
-
-  // handle new messages
-  socket.on('new:message', function (msgObject) {
-    io.emit('new:message', msgObject);
-  });
-
-  // handle new members
-  socket.on('new:member', function (name) {
-    io.emit('new:member', name);
-  });
-});
-
 server.listen(port, function(){
   console.log('listening on port ' + port);
+  
+  io.on('connection', function (socket) {
+    console.log("USER CONNECTED...");
+
+    // handle new messages
+    socket.on('new:message', function (msgObject) {
+      io.emit('new:message', msgObject);
+    });
+
+    // handle new members
+    socket.on('new:member', function (name) {
+      io.emit('new:member', name);
+    });
+  });
 });
