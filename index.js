640d6db50c868672e4e46bed37ddbddd529a0757
Patrick Ayres <briceayres@gmail.com>
2017-08-28 22:10:48 -0500

    FIRST BLOOD

---
+++ index.js
@@ -0,0 +1,34 @@
+
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+io.on('connection', function (socket) {
+  console.log("USER CONNECTED...");
+
+  // handle new messages
+  socket.on('new:message', function (msgObject) {
+    io.emit('new:message', msgObject);
+  });
+
+  // handle new members
+  socket.on('new:member', function (name) {
+    io.emit('new:member', name);
+  });
+});
+
+server.listen(port, function(){
+  console.log('listening on port ' + port);
+});
---
+++ public/javascript/chat.js
@@ -0,0 +1,71 @@
+$(function () {
+  let socket = io();
+  let name = '';
+  let nameInput = $('#name-input');
+  let chatInput = $('#chat-input');
+
+  // handle name entered with via keyboard enter
+  nameInput.keydown(function(event) {
+    if ( event.which == 13 ) {
+      event.preventDefault();
+
+      // ensure message not empty
+      if (nameInput.val() !== '') {
+        name = nameInput.val();
+        nameInput.val('');
+        $('.enter-name').hide();
+        socket.emit('new:member', name);
+      }
+    }
+  });
+
+  // handle name entered with when clicking enter
+  $('.submit-name').on('click', function(event) {
+    event.preventDefault();
+
+    // ensure message not empty
+    if (nameInput.val() !== '') {
+      name = nameInput.val();
+      nameInput.val('');
+      $('.enter-name').hide();
+      socket.emit('new:member', name);
+    }
+  });
+
+
+  // handle keyboard enter button being pressed
+  chatInput.keydown(function(event) {
+    if ( event.which == 13 ) {
+      event.preventDefault();
+
+      // ensure message not empty
+      if (chatInput.val() !== '' && name !== '') {
+        socket.emit('new:message', {name: name, msg: chatInput.val()});
+        chatInput.val('');
+      }
+    }
+  });
+
+  // handle submit chat message button being clicked
+  $('.submit-chat-message').on('click', function(event) {
+    event.preventDefault();
+
+    // ensure message not empty
+    if (chatInput.val() !== '' && name !== '') {
+      socket.emit('new:message', {name: name, msg: chatInput.val()});
+      chatInput.val('');
+    }
+  });
+
+  // handle receiving new messages
+  socket.on('new:message', function(msgObject){
+    $('#messages').append($('<div class="msg new-chat-message">').html('<span class="member-name">' + msgObject.name + '</span>: ' + msgObject.msg));
+    window.scrollTo(0, document.body.scrollHeight);
+  });
+
+  // handle members joining
+  socket.on('new:member', function(name){
+    $('#messages').append($('<div class="msg new-member">').text(name + ' has joined the room'));
+    window.scrollTo(0, document.body.scrollHeight);
+  });
+});
